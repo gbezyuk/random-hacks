d9589c7f39ad14e893e4c0b410231c9035b59b5d
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-12-23 01:42:26 +0300

    computational-geometry: convex combination tabulation; now yielding instead of printing

--- computational-geometry/code-samples.py
+++ computational-geometry/code-samples.py
@@ -13,6 +13,7 @@ p2 = Point(-3.0, -2.0)
 
 def tabulate_convex_combination (p1, p2, tabulation_step=0.1):
     for alpha in frange(0.0, 1.0, tabulation_step):
-        print alpha, 1.0 - alpha, p1.x * alpha + (1.0 - alpha) * p2.x, p1.y * alpha + (1.0 - alpha) * p2.y
+        yield alpha, 1.0 - alpha, p1.x * alpha + (1.0 - alpha) * p2.x, p1.y * alpha + (1.0 - alpha) * p2.y
 
-tabulate_convex_combination(p1, p2)
+for data in tabulate_convex_combination(p1, p2):
+    print data
