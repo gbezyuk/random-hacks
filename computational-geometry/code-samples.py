b6b3cc72bcb96d43f2dcb2dc20aaee8d962b5a73
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-12-23 01:49:38 +0300

    computational-geometry: cross-product

--- computational-geometry/code-samples.py
+++ computational-geometry/code-samples.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def frange(x, y, tabulation_step):
   while x < y:
     yield x
@@ -8,12 +10,31 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return "(%s, %s)" % (self.x, self.y)
+
+    def __repr__(self):
+        return "(%s, %s)" % (self.x, self.y)
+
+    @property
+    def abs(self):
+        return sqrt(self.x * self.x + self.y * self.y)
+
+Vector = Point;
+
 p1 = Point( 2.0,  3.0)
 p2 = Point(-3.0, -2.0)
+
 
 def tabulate_convex_combination (p1, p2, tabulation_step=0.1):
     for alpha in frange(0.0, 1.0, tabulation_step):
         yield alpha, 1.0 - alpha, p1.x * alpha + (1.0 - alpha) * p2.x, p1.y * alpha + (1.0 - alpha) * p2.y
 
-for data in tabulate_convex_combination(p1, p2):
-    print data
+
+def cross_product(v1, v2):
+    return Vector(v1.x * v2.y, v1.y * v2.x)
+
+v3 = cross_product(p1, p2)
+print(p1, p1.abs)
+print(p2, p2.abs)
+print(v3, v3.abs)
