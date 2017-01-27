aefdd9b06e1b6ba5c137f0554a652f5121dab84c
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-01-28 01:42:56 +0300

    functional-as-fuck-style solution

--- checkio/grille_cypher.py
+++ checkio/grille_cypher.py
@@ -1,12 +1,16 @@
 def rotate_grille_clockwise (grille):
     return zip(*grille[::-1])
 
-def recall_password(cipher_grille, ciphered_password):
+def generate_all_full_product_matrix(gl):
+    return zip([0]*gl+[1]*gl+[2]*gl+[3]*gl, range(gl)*gl)
+
+def recall_password(cipher_grille, ciphered_password, number_of_turns=4):
     res = ''
-    for k in range(4):
-        for i in range(len(cipher_grille)):
-            res += ''.join([ciphered_password[i][j] for j in range(len(cipher_grille[i])) if cipher_grille[i][j] == 'X'])            
-        
+    gl = len(cipher_grille)    
+    for k in range(number_of_turns):
+        res += ''.join([
+            ciphered_password[i][j] for i, j in generate_all_full_product_matrix(gl) if cipher_grille[i][j] == 'X'
+        ])
         cipher_grille = rotate_grille_clockwise(cipher_grille)
     return res
 
@@ -25,5 +29,6 @@ cypher = (
 )
 
 password = 'icantforgetiddqd'
+print(recall_password(grille, cypher))
 
 assert(recall_password(grille, cypher) == password)
